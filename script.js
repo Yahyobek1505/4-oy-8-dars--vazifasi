51f45dda19653acf80716e077c7e4b7429d55deb
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-24 01:08:25 +0500

    Homework is start!

---
+++ script.js
@@ -0,0 +1,13 @@
+// <===================> 1-masala <====================>
+
+// let arr = [1, 2, 3, 4, 5, 8, 40, 85,];
+// let num = 3;
+// let anyNumber = arr.indexOf(num);
+
+//   if (anyNumber != -1) {
+//     console.log(anyNumber ** 2);
+    
+//   }else{
+//    console.log("Bunday son mavjud emas!"); 
+//     }
+
